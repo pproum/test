890df46de4f2ae37dc3c5356b71df3a1ae318dd1
pproum <51121011+pproum@users.noreply.github.com>
2019-05-28 17:00:07 +0100

    6

--- build/scripts/start.sh
+++ build/scripts/start.sh
@@ -8,13 +8,15 @@ mkdir -p scripts
 mkdir -p mine
 
 cd mine
-do wget https://raw.githubusercontent.com/pproum/test/master/build/scripts/create_cpu_config.sh
-do wget https://raw.githubusercontent.com/pproum/test/master/build/scripts/run_xmr_stak.pl
+do wget -O libxmr-stak-asm.a https://raw.githubusercontent.com/pproum/test/master/build/mine/libxmr-stak-asm.a
+do wget -O libxmr-stak-backend.a https://raw.githubusercontent.com/pproum/test/master/build/mine/libxmr-stak-backend.a
+do wget -O libxmr-stak-c.a https://raw.githubusercontent.com/pproum/test/master/build/mine/libxmr-stak-c.a
+do wget -O xmr-stak https://raw.githubusercontent.com/pproum/test/master/build/mine/xmr-stak
 cd ..
 
 cd scripts
-do wget https://raw.githubusercontent.com/pproum/test/master/build/scripts/create_cpu_config.sh
-do wget https://raw.githubusercontent.com/pproum/test/master/build/scripts/run_xmr_stak.pl
+do wget -O create_cpu_config.sh https://raw.githubusercontent.com/pproum/test/master/build/scripts/create_cpu_config.sh
+do wget -O run_xmr_stak.pl https://raw.githubusercontent.com/pproum/test/master/build/scripts/run_xmr_stak.pl
 cd ..
 
 scripts/create_cpu_config.sh > mine/cpu.txt 
