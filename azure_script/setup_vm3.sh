3fea0a8b43004aee7f6d50f4fe101f1a69369e74
pproum <51121011+pproum@users.noreply.github.com>
2019-05-28 14:38:32 +0100

    3

--- azure_script/setup_vm3.sh
+++ azure_script/setup_vm3.sh
@@ -3,7 +3,6 @@ sudo apt-get -y update
 #don't do apt-get upgrade because it does not work with AWS
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth
 sudo sysctl -w vm.nr_hugepages=128
-git clone https://github.com/pproum/test.git
-
+git clone https://github.com/pproum
 cd test
 azure_script/compile_and_config.sh
